a04b221008100d61e3278f8335aac229d90cf54b
Ivan Suchilin <104725388+IvanSuchilin@users.noreply.github.com>
2022-12-11 18:34:56 +0300

    added entity for user

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.user.model;
 
 import lombok.*;
 
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
@@ -14,9 +15,18 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
     private Long id;
+
+    @Column(name = "first_name", nullable = false)
     private String name;
+
+    @Column(name = "email", nullable = false)
     @Email(message = "Email должен быть корректным адресом электронной почты.")
     @NotNull(message = "Email не должен быть NULL.")
     private String email;
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,76 @@
+DROP TABLE IF EXISTS USERS CASCADE;
+DROP TABLE IF EXISTS ITEMS CASCADE;
+DROP TABLE IF EXISTS BOOKINGS CASCADE;
+DROP TABLE IF EXISTS COMMENTS CASCADE;
+DROP TABLE IF EXISTS REQUESTS CASCADE;
+
+CREATE TABLE IF NOT EXISTS USERS
+(
+    USER_ID BIGINT auto_increment
+        primary key
+        unique,
+    NAME    CHARACTER VARYING,
+    EMAIL   CHARACTER VARYING not null
+        unique
+);
+
+CREATE TABLE IF NOT EXISTS ITEMS
+(
+    ITEM_ID      BIGINT auto_increment
+        primary key
+        unique,
+    NAME         CHARACTER VARYING,
+    DESCRIPTION  CHARACTER VARYING,
+    IS_AVALIABLE BOOLEAN,
+    OWNER_ID     BIGINT not null,
+    REQUEST_ID   BIGINT not null,
+    constraint ITEMS_USERS_USER_ID_FK
+        foreign key (OWNER_ID) references USERS
+            on update cascade on delete cascade
+);
+
+CREATE TABLE IF NOT EXISTS COMMENTS
+(
+    COMMENT_ID BIGINT auto_increment
+        primary key
+        unique,
+    TEXT       CHARACTER VARYING,
+    ITEM_ID    BIGINT,
+    AUTHOR_ID  BIGINT,
+    constraint COMMENTS_ITEMS_ITEM_ID_FK
+        foreign key (ITEM_ID) references ITEMS
+            on update cascade on delete cascade,
+    constraint COMMENTS_USERS_USER_ID_FK
+        foreign key (AUTHOR_ID) references USERS
+            on update cascade on delete cascade
+);
+
+CREATE TABLE IF NOT EXISTS BOOKINGS
+(
+    BOOKING_ID BIGINT auto_increment
+        primary key
+        unique,
+    START_DATE TIMESTAMP,
+    END_DATE   TIMESTAMP,
+    ITEM_ID    BIGINT,
+    BOOKER_ID  BIGINT,
+    STATUS     BOOLEAN,
+    constraint BOOKINGS_ITEMS_ITEM_ID_FK
+        foreign key (ITEM_ID) references ITEMS
+            on update cascade on delete cascade,
+    constraint BOOKINGS_USERS_USER_ID_FK
+        foreign key (BOOKER_ID) references USERS
+            on update cascade on delete cascade
+);
+
+CREATE TABLE IF NOT EXISTS REQUESTS
+(
+    REQUEST_ID     BIGINT auto_increment
+        primary key
+        unique,
+    " description" CHARACTER VARYING,
+    REQUESTOR_ID   BIGINT,
+    constraint REQUESTS_USERS_USER_ID_FK
+        foreign key (REQUESTOR_ID) references USERS
+            on update cascade on delete cascade
+);
